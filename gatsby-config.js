3d957e9a3a7bd32bccda39edaa75d698e0bddaaf
Jason Lengstorf <jason@lengstorf.com>
2021-06-03 10:04:51 -0700

    wip: playing with Gatsby Functions on Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Functions on Netlify",
+    title: 'Gatsby Functions on Netlify',
+  },
+  flags: {
+    FUNCTIONS: true,
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
   ],
 };
